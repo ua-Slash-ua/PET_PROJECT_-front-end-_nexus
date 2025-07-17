77134f30e478a5cc84f335af2bbf8b3f0926ebed
ua-Slash-ua <so.slashua@gmail.com>
2025-07-17 13:46:19 +0300

    Editing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header/Header";
+import HeroBlock from "@/components/HeroBlock/HeroBlock";
 
 export default function Home() {
   return (
       <>
           <Header/>
+          <HeroBlock/>
       </>
   );
 }
