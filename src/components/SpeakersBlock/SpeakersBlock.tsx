73b147a4dc596b9b09854a2a24cc352ed332cdac
ua-Slash-ua <so.slashua@gmail.com>
2025-07-18 17:12:20 +0300

    Editing speakers block

--- src/components/SpeakersBlock/SpeakersBlock.tsx
+++ src/components/SpeakersBlock/SpeakersBlock.tsx
@@ -1,4 +1,6 @@
 import styles from './SpeakersBlock.module.css'
+import {SpeakersData} from "@/data/speakers.data";
+import Image from "next/image";
 
 export default function SpeakersBlock() {
     return (<>
@@ -9,11 +11,51 @@ export default function SpeakersBlock() {
                         KEYNOTE <br/>
                         SPEAKERS
                     </span>
-                    <span>
+                <span>
                         Meet the industry leaders <br/>
                         shaping the future of AI.
                     </span>
             </header>
+            <div className={styles.speakers_container}>
+
+                {SpeakersData.map((speaker, index) => (
+                    index <= 1 ? (
+                        <div className={styles.speaker_item} key={speaker.id}>
+                            <div className={styles.speaker_data}>
+                                <span>{speaker.name}</span>
+                                <span style={{whiteSpace: 'pre-line'}}>{speaker.position}</span>
+                            </div>
+                            <div className={styles.speaker_photo}>
+                                <Image
+                                    className={styles.greyImage}
+                                    src={speaker.photo}
+                                    alt={speaker.name}
+                                    width={295}
+                                    height={452}
+                                />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={styles.speaker_item} key={speaker.id}>
+                            <div className={styles.speaker_photo}>
+                                <Image
+                                    className={styles.greyImage}
+                                    src={speaker.photo}
+                                    alt={speaker.name}
+                                    width={295}
+                                    height={452}
+                                />
+                            </div>
+                            <div className={styles.speaker_data}>
+                                <span>{speaker.name}</span>
+                                <span style={{whiteSpace: 'pre-line'}}>{speaker.position}</span>
+                            </div>
+                        </div>
+                    )
+                ))}
+
+
+            </div>
         </section>
     </>)
 }
--- src/data/speakers.data.ts
+++ src/data/speakers.data.ts
@@ -1,3 +1,26 @@
-export const SpeakersData = {
-
-}
+export const SpeakersData = [
+    {
+        id: 0,
+        name: "Dr. Emily Carter",
+        position: "Chief AI Scientist, OpenAI",
+        photo:"/speakers/Image (22).png"
+    },
+    {
+        id: 1,
+        name: "Elon Park",
+        position: "CTO, DeepMind \n",
+        photo:"/speakers/Image (23).png"
+    },
+    {
+        id: 2,
+        name: "Laura Kim",
+        position: "AI Policy Advisor, EU Commission",
+        photo:"/speakers/Image (24).png"
+    },
+    {
+        id: 3,
+        name: "Dr. Alan Foster",
+        position: "Stanford AI Lab \n",
+        photo:"/speakers/Image (25).png"
+    },
+]
