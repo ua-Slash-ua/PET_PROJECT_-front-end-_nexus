9eb26cb05c93808b95eb8b4862f06bd9352e1d11
ua-Slash-ua <so.slashua@gmail.com>
2025-07-18 11:48:45 +0300

    Add btnHero

--- src/components/HeroBlock/HeroBlock.tsx
+++ src/components/HeroBlock/HeroBlock.tsx
@@ -1,4 +1,5 @@
 import styles from './HeroBlock.module.css'
+import BtnHero from "@/components/layouts/Buttons/BtnHero/BtnHero";
 
 export default function HeroBlock() {
     return (
@@ -28,14 +29,7 @@ export default function HeroBlock() {
                             Explore the cutting-edge innovations shaping the future of artificial intelligence. <br/>
                             Join global leaders and visionaries for two days of insights, discussions, and breakthroughs.
                         </div>
-                        <div className={styles.btn_tickets}>
-                            <span>Early bird tickets</span>
-                            <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd" clipRule="evenodd"
-                                      d="M4.89715 1.65427C4.89715 1.01678 5.41394 0.5 6.05142 0.5H15.8457C16.4832 0.5 17 1.01678 17 1.65427V11.4486C17 12.0861 16.4832 12.6028 15.8457 12.6028C15.2082 12.6028 14.6915 12.0861 14.6915 11.4486V4.44092L1.97046 17.1619C1.51969 17.6127 0.788848 17.6127 0.338078 17.1619C-0.112693 16.7112 -0.112693 15.9803 0.338078 15.5295L13.0591 2.80854H6.05142C5.41394 2.80854 4.89715 2.29176 4.89715 1.65427Z"
-                                      fill="white"/>
-                            </svg>
-                        </div>
+                        <BtnHero text={'Early bird tickets'}/>
                     </div>
             </section>
 
