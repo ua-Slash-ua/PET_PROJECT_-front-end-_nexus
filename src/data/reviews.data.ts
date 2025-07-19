a178f74b682cd93f832e9a4e12e25477bbba8d90
ua-Slash-ua <so.slashua@gmail.com>
2025-07-19 14:34:45 +0300

    Add review block

---
+++ src/data/reviews.data.ts
@@ -0,0 +1,26 @@
+export const review = [
+    {
+        id:0,
+        name: 'Elena Rojas',
+        position:'AI Researcher, DeepMind',
+        review:'"Incredible speakers, top-tier networking, and cutting-edge discussions all in one place."',
+        title:'THE BEST AI EVENT!',
+        photo:"/speakers/Image (22).png"
+    },
+    {
+        id:2,
+        name: 'Mark Vandenberg',
+        position:'CTO, NeuralTech',
+        review:'"This summit opened my eyes to the future of AI and how it will shape industries."',
+        title:'GAME-CHANGING INSIGHTS',
+        photo:"/speakers/Image (23).png"
+    },
+    {
+        id:3,
+        name: 'David Laurent',
+        position:'CEO, FutureAI Labs',
+        review:'"From hands-on workshops to visionary talks, this summit is a must-attend for AI professionals."',
+        title:'UNMATCHED OPPORTUNITIES',
+        photo:"/speakers/Image (24).png"
+    }
+]
