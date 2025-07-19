8e97c1059e9cb71577a2a3073b04219b80b765b0
ua-Slash-ua <so.slashua@gmail.com>
2025-07-19 14:30:08 +0300

    Close WhuAttend block

--- src/components/WhyAttendBlock/WhyAttendBlock.tsx
+++ src/components/WhyAttendBlock/WhyAttendBlock.tsx
@@ -1,4 +1,5 @@
 import styles from './WhyAttendBlock.module.css'
+import {whyAttendsData} from "@/data/why_attends.data";
 
 export default function WhyAttendBlock() {
     return (
@@ -12,6 +13,33 @@ export default function WhyAttendBlock() {
                         Discover why Next-Gen AI Summit is the must-attend event for AI professionals, innovators, and industry leaders.
                     </span>
                 </header>
+                <div className={styles.why_attend_content}>
+                    {
+                        whyAttendsData.map((whyAttend, index) => (
+                            index <= 1?
+
+                            (<div key={index} className={styles.why_attend_item}>
+                                <span className={styles.number}>{whyAttend.id}</span>
+                                <span className={styles.title}>{whyAttend.title}</span>
+                                <div>
+                                    <span >{whyAttend.description}</span>
+                                </div>
+
+
+                            </div>):(
+                                    <div key={index} className={`${styles.why_attend_item} ${styles.reverse}`}>
+                                        <span className={styles.number}>{whyAttend.id}</span>
+                                        <span className={styles.title}>{whyAttend.title}</span>
+                                        <div>
+                                            <span >{whyAttend.description}</span>
+                                        </div>
+
+
+                                    </div>
+                                )
+                        ))
+                    }
+                </div>
             </section>
         </>
     )
--- src/data/why_attends.data.ts
+++ src/data/why_attends.data.ts
@@ -2,21 +2,21 @@ export const whyAttendsData = [
     {
         id: '01',
         title: 'Cutting-Edge Insights',
-        description: 'Gain firsthand knowledge from top AI experts and pioneers shaping the industry.'
+        description: 'Gain firsthand knowledge from top AI experts and pioneers shaping the industry.',
     },
     {
-        id: '01',
+        id: '02',
         title: 'Hands-On Learning',
-        description: 'Participate in interactive workshops, live demos, and deep-dive sessions to sharpen your skills.'
+        description: 'Participate in interactive workshops, live demos, and deep-dive sessions to sharpen your skills.',
     },
     {
-        id: '01',
+        id: '03',
         title: 'Exclusive Networking',
-        description: 'Connect with AI leaders, investors, startups, and fellow professionals at curated networking events.'
+        description: 'Connect with AI leaders, investors, startups, and fellow professionals at curated networking events.',
     },
     {
-        id: '01',
+        id: '04',
         title: 'Innovation Showcase',
-        description: 'Explore groundbreaking AI solutions, from emerging startups to tech giants redefining the future.'
+        description: 'Explore groundbreaking AI solutions, from emerging startups to tech giants redefining the future.',
     },
 ]
