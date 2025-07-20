ed524674301499d9adef62614878c77336f7fa1e
ua-Slash-ua <so.slashua@gmail.com>
2025-07-20 12:23:44 +0300

    Add header Timer block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import DescriptionBlock from "@/components/DescriptionBlock/DescriptionBlock";
 import SpeakersBlock from "@/components/SpeakersBlock/SpeakersBlock";
 import WhyAttendBlock from "@/components/WhyAttendBlock/WhyAttendBlock";
 import ReviewBlock from "@/components/ReviewBlock/ReviewBlock";
+import TimerBlock from "@/components/TimerBlock/TimerBlock";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
           <SpeakersBlock/>
           <WhyAttendBlock/>
           <ReviewBlock/>
+          <TimerBlock/>
       </>
   );
 }
---
+++ src/components/TimerBlock/TimerBlock.tsx
@@ -0,0 +1,23 @@
+import styles from './TimerBlock.module.css'
+
+export default function TimerBlock() {
+    return (<>
+
+        <section className={styles.section_timer} id="timer">
+            <header className={styles.timer_header}>
+                    <span>
+                        REGISTER
+                        NOW
+                    </span>
+                <span>
+                        Secure your spot at the Next-Gen AI Summit 2052 and join the future of AI innovation.
+                    </span>
+            </header>
+            <div className={styles.timer_container}>
+
+                
+            </div>
+
+        </section>
+    </>)
+}
